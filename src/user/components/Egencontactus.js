beb6ec620c41e587b6b1272e424b74601de9d9f2
Christoffer Bukdahl Danielsen <chri541x@videndjurs.net>
2020-11-08 14:19:02 +0100

    hey

--- src/user/components/Egencontactus.js
+++ src/user/components/Egencontactus.js
@@ -1,6 +1,22 @@
 import React  from "react";
+import { sendKontaktbesked } from "../../admin/helpers/APIkald/apikald";
 
 const Egencomputer = () => {
+
+
+  const handleBesked = (e) => {
+    e.preventDefault();
+
+    sendKontaktbesked(e.target).then((response) => {
+      if (response !== "error") {
+
+        alert("Din besked er sendt tak" );
+        
+      }
+    });
+  };
+  
+
   return (
     <div className="row col-12  containerd ">
         <div className="col-12 d">
@@ -8,18 +24,18 @@ const Egencomputer = () => {
         <p className="col-11  my-1 ">Total</p>
         <div className="col-1 my-1">$0</div>
       </div> 
-<form >
+<form onChange={handleBesked} >
       <div className="form-group row m-auto">
             <h4 className="col-12 m-2  whitet">Contact us!</h4>
 
 
         
         <div className="col-12 col-lg-6 my-1 m-sm-0 ">
-    <input type="text" className="form-control col-12 g" placeholder="First name" /></div>
+    <input name="name" type="text" className="form-control col-12 g" placeholder="First name" /></div>
     <div className="col-12 my-1 my-sm-0 col-lg-6">
-    <input type="text" className="form-control col-12 g " placeholder="Last name"  /></div>
+    <input name="name" type="text" className="form-control col-12 g " placeholder="Last name"  /></div>
     <div className="col-12">
-      <input type="email" className="form-control col-12 g pl-3 "  placeholder="email" /> 
+      <input name="email" type="email" className="form-control col-12 g pl-3 "  placeholder="email" /> 
     
       <button type="submit" className="btn btn-dark mt-2">Send</button>
 </div>
